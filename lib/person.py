c64e09fb5fe65faf67376723b09e9ef10d22d1bd
MiriamRehema <rehemamiriam30@gmail.com>
2023-08-21 12:48:31 +0300

    pyhton-p3-instance-methods

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,14 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
-    pass
+person1=Person()
+person1.talk()
+
+person2=Person()
+person2.walk()
+
